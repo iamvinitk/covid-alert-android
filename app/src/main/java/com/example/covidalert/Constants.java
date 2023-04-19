710bfea435f39c6597d579b6cb524b23482d941a
Vinit Kanani <kananivinit97@gmail.com>
2023-04-18 20:44:22 -0700

    minor changes

--- app/src/main/java/com/example/covidalert/Constants.java
+++ app/src/main/java/com/example/covidalert/Constants.java
@@ -1,5 +1,5 @@
 package com.example.covidalert;
 
 public class Constants {
-    public static final String BASE_URL = "https://4e4f-2a09-bac5-6212-1232-00-1d0-2d.ngrok-free.app";
+    public static final String BASE_URL = "https://a8e2-2a09-bac5-6213-1232-00-1d0-4e.ngrok-free.app";
 }
